8c13bda0ccfdd79d08407bd3a325dd7ade07fe81
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-03-27 19:35:12 -0400

    chore(docs): change main example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chromadb_rs::client::{ChromaClient, ChromaClientParams};
-use std::{error::Error, result::Result};
+use std::{error::Error, result::Result, collections::HashMap};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -13,7 +13,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("{hb}");
 
-    let new_collection = client.create_collection("creation-test", None).await?;
+    let new_collection = client
+        .create_collection(
+            "crea",
+            Some(HashMap::from([
+                (
+                    "description".to_string(),
+                    "testing collection creation".to_string(),
+                ),
+                ("wtf".to_string(), "hard to deserialize...".to_string()),
+            ])),
+        )
+        .await?;
 
     println!("{:?}", new_collection);
 
