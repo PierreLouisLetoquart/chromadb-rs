5d43fbdce0e3f4caccfc0faf54436f2fbc50bb0d
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-03-27 15:45:24 -0400

    chore(docs): crates.io documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
+//! # Chroma-rs
+//!
+//! The unofficial lib for [Chroma](https://docs.trychroma.com/) in rust. Chroma-rs uses endpoints of the [chroma db backend](https://docs.trychroma.com/api#backend-api).
+//!
+//! Currently the lib is a work in progress... check out the [github repo](https://github.com/PierreLouisLetoquart/chroma-rs) to contribute.
+//!
+//! ## Chroma client
+//!
+//! **Creates a new ChromaClient instance:**
+//!
+//! ```rust
+//! use chroma_rs::{ChromaClient, ChromaClientParams};
+//!
+//! let client = ChromaClient::new(ChromaClientParams {
+//!     host: "localhost".to_string(),
+//!     port: "8000".to_string(),
+//!     ssl: false,
+//! });
+//! ```
+//!
+
 pub mod client;
